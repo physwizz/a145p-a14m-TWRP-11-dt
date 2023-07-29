50a2762f3df5851b9926b7ca5dc4f365a6dca96e
For Fork Sake <sakeforfork@gmail.com>
2023-07-29 06:06:52 +0100

    Touchscreen
    Vendor bin/libs added [ Failed ]
    GT9886 - firmware files placed in  vendor/firmware + BoardConfig Adjustments [ Failed ]
     - Kernel Adjustment Required.
    Recompiled with Adjusted Kernel (touchscreen drivers) [ Working!!]
    
    - Several mount errors resolved
    - init.recovery.usb.rc added and adjusted, allowing ADB/MTP to work together
    - prepped BoardConfig for future A13 Crypto development.

--- device.mk
+++ device.mk
@@ -1,8 +1,9 @@
-#
-# Copyright (C) 2023 The Android Open Source Project
-# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-
 LOCAL_PATH := device/samsung/a14m
+
+# Dynamic partitions
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+# fastbootd
+PRODUCT_PACKAGES += \
+    android.hardware.fastboot@1.0-impl-mock \
+    fastbootd
